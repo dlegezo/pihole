ac2bf45f9a51b144760b111b3f4b5883bcd40368
Denis Legezo <denis.legezo@kaspersky.com>
2021-04-25 17:38:36 +0300

    Speed up with one request with entities

--- get_dns.py
+++ get_dns.py
@@ -16,54 +16,63 @@ import pprint as pp
 import sqlite3
 import requests
 
-# connections block
 # --------------------------------------------------------------------------
+# connect to pihole
 def connect_pihole_api():
-    handle = ph.PiHole(creds.pihole_ip)
-    handle.authenticate(creds.pihole_pass)
-    return handle
+    conn = ph.PiHole(creds.pihole_ip)
+    conn.authenticate(creds.pihole_pass)
+    return conn
 
 def connect_pihole_sql():    
     conn = sqlite3.connect(creds.pihole_db_path)
     return conn
 
-# requests to databases
 # --------------------------------------------------------------------------
+# get domains from pihole
 # todo - filter out visited with last 24 hours
-def get_domains_api(handle):
-    handle.refresh()
-    unique = handle.unique_domains
+def get_domains_api(conn):
+    conn.refresh()
+    unique = conn.unique_domains
     pp.pprint("Host visited " + unique + " unique domains")
-    handle.refreshTop(unique)
-    check_domain_aptdb(handle.top_queries.items())    
+    conn.refreshTop(unique)
+    check_domain_aptdb(conn.top_queries.items())    
 
 def get_domains_sql(conn):        
     sql = "select domain from queries where timestamp>=strftime('%s','now')-" + creds.time_frame + " group by domain"    
     cur = conn.cursor()
     return cur.execute(sql)    
 
+# --------------------------------------------------------------------------
+# check agains another database
 # todo - wrap HTTP POST requests into separate lib as pihole-api
 def check_domain_aptdb(domains):    
-    for domain in domains:                
+    entities = '['        
+    for i, domain in enumerate(domains):                        
         try:
-            print(domain[0])
-            body = dict(auth_code=creds.apt_db_key, entries='[\"' + domain[0] + '\"]')                
-            r = requests.post(creds.apt_db_url, body, auth=(creds.apt_db_htuser,creds.apt_db_htpass))                
-            print(r.text)    
+            if i == 0:
+                entities += "\"" + domain[0] + "\""
+            else:
+                entities += ",\"" + domain[0] + "\""            
         except TypeError:
             print("Domains list is over")
+    # to test with tagged one
+    entities += ',\"​azussystems.com\"]'
+    body = dict(auth_code=creds.apt_db_key, entries=entities)                
+    r = requests.post(creds.apt_db_url, body, auth=(creds.apt_db_htuser,creds.apt_db_htpass))                
+    print(r.text)
+    print(entities)
 
 # --------------------------------------------------------------------------
 if __name__ == "__main__":
-    # sql way if file is available
-    print("Through SQLite")
+    # SQLite way
+    print("Using SQLite")
     conn = connect_pihole_sql()
     domains = get_domains_sql(conn)    
     check_domain_aptdb(domains)
     conn.close()
 
     # HTTP API way
-    print("Through HTTP API")
+    print("Using HTTP API")
     handle = connect_pihole_api()
     domains = get_domains_api(handle)
     check_domain_aptdb(domains)
